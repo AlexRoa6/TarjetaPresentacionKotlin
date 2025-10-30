97b3f399d12646a3f3a0bce0bbb1f0f5a8d4766c
AlexRoa6 <alexroasebastian9@gmail.com>
2025-10-30 11:58:47 +0100

    Añadir parte de abajo y crear funcion que contiene las demas

--- app/src/main/java/com/example/p1_tarjetapresentacion_jorge_marcos_alejandro/MainActivity.kt
+++ app/src/main/java/com/example/p1_tarjetapresentacion_jorge_marcos_alejandro/MainActivity.kt
@@ -5,18 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
@@ -32,10 +38,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    ImagenPrincipal(
-                        painter = painterResource(R.drawable.kotlin),
-
-                        )
+                    ComposeTarjetaPresentacionApp()
                 }
             }
         }
@@ -43,30 +46,73 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun ImagenPrincipal(painter: Painter) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize() // Ocupa toda la pantalla
-    ) {
+    Box(modifier = Modifier
+        .fillMaxSize()) {
         Image(
             painter = painter,
             contentDescription = null,
             modifier = Modifier
                 .size(320.dp) // tamaño de la imagen
-                .align(Alignment.TopCenter) // centramos
-                .padding(top = 2.dp), // subimos la img
-            contentScale = ContentScale.Crop
+                .align(Alignment.Center) // centramos
         )
     }
 }
 
 
+@Composable
+fun ParteAbajo(){
+    Column (modifier = Modifier.padding(bottom = 16.dp),
+        verticalArrangement = Arrangement.Bottom,
+        ) {
+        ImagenEInformacionDeAbajo(
+            painterResource(R.drawable.telefono),
+            stringResource(R.string.numeroTelefono)
+        )
+        ImagenEInformacionDeAbajo(
+            painterResource(R.drawable.email),
+            stringResource(R.string.emailEmpresa)
+        )
+        ImagenEInformacionDeAbajo(
+            painterResource(R.drawable.email),
+            stringResource(R.string.emailPersona)
+        )
 
+    }
+}
+@Composable
+fun ImagenEInformacionDeAbajo(imagenPainter: Painter, informacion: String){
+    Row (modifier = Modifier.padding(end = 8.dp).padding(bottom = 24.dp)){
+        Image(
+            painter = imagenPainter,
+            contentDescription = null,
+            modifier = Modifier.size(24.dp)
+        )
+        Text(
+            text = informacion,
+            fontSize = 24.sp
+        )
+    }
 
+}
+
+@Composable
+fun ComposeTarjetaPresentacionApp(){
+    Box(modifier = Modifier
+        .fillMaxSize()) {
+
+        ImagenPrincipal(
+            painter = painterResource(R.drawable.kotlin)
+        )
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter) {
+            ParteAbajo()
+        }
+    }
+}
 @Preview
 @Composable
-fun VerImagen(){
+fun TarjetaPresentacionAppPreview(){
     P1_TarjetaPresentacion_Jorge_Marcos_AlejandroTheme {
-        ImagenPrincipal(painter = painterResource(R.drawable.kotlin)
-        )
+        ComposeTarjetaPresentacionApp()
     }
 }
